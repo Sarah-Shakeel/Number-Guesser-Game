66d9a9436c765bfdc2d55f7df80fe80e317e1879
Sarah-Shakeel <44284967+Sarah-Shakeel@users.noreply.github.com>
2020-05-12 02:34:38 +0500

    my own number guesser game game version, maybe some additional work can be done in it.

--- script.js
+++ script.js
@@ -1,144 +1,62 @@
-// /*Project 'Number Guesser' from codecademy pro after lesson of Variables, 
-// conditionals and functions */
-// let humanScore = 0;
-// let computerScore = 0;
-// let currentRoundNumber = 1;
-
-// //Write your code below:
-
-// /* It is a number guessing game. When the game starts, there is a secret number
-// chosen from 0 to 9. which is unknown to both players; user and computer. Now, 
-// the user will choose a number and computer will choose a number. Whose number 
-// will be closest to the secret number, he will be the winner i.e. either the 
-// user(playing this game) or computer. */
-
-// //generating secret number randomly
-// const generateTarget = () =>
-// {
-//   return Math.floor(Math.random()*10);
-// }
-
-// //declare and define human guess, comp guess and secret number
-// let humanGuess = 5;
-// console.log(humanGuess);
-// let computerGuess = Math.floor(Math.random() * 10);
-// console.log(computerGuess);
-// let secretNumber = generateTarget();
-// console.log(secretNumber);
-
-// //compare guesses function to determine winner
-// const compareGuesses = (humanGuess,computerGuess,secretNumber) =>
-// {   
-//  //checking that who is winner by Math.absolute method 
-//   if((Math.abs(humanGuess-secretNumber)) <= (Math.abs(computerGuess-secretNumber)))
-//     {
-      
-//       return true; // User guess is closer to secret number
-//     }
-//   else 
-//     {
-//       return false;  //Computer guess is closer to secret number
-//     }
-  
-// }
-// console.log(compareGuesses(humanGuess,computerGuess,secretNumber));
-
-// //updating score after determining the winner
-// const updateScore = (winner) =>
-// {
-//   if(compareGuesses)
-//     {
-//       humanScore++;
-//       return 'Your score  is ' +humanScore+ '.';
-//     }
-//   else
-//     {
-//       computerScore++;
-//       return 'Computer score is ' +computerScore+ '.';
-//     }
-// }
-// console.log(updateScore());
-// console.log(humanScore);
-// console.log(computerScore)
-
-
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+//Write your code below:
+// It is a number guessing game. When the game starts, there is a secret number chosen from 0 to 9. which is unknown to both players; user and computer. Now, the user will choose a number and computer will choose a number. Whose number will be closest to the secret number, he will be the winner i.e. either the user(playing this game) or computer.
 
+//generating secret number randomly
+const generateTarget = () =>
+{
+  return Math.floor(Math.random()*10);
+}
+//input human guess, and generating computer guess and secret number plus printing them to console so that player knows his number, comp guess and secret number.
+let humanGuess = 5;
+console.log(humanGuess);
+let computerGuess = Math.floor(Math.random() * 10);
+console.log(computerGuess);
+let secretNumber = generateTarget();
+console.log(secretNumber);
+//calculating user distance and computer distance from secret number by Math.absolute method
+// let userDistance = Math.abs(humanGuess-secretNumber);
+// let compDistance = Math.abs(computerGuess-secretNumber);
 
+//compare guesses function to determine winner
+const compareGuesses = (humanGuess,computerGuess,secretNumber) =>
+{   
+ //checking that who is winner by Math.absolute method 
+  if((Math.abs(humanGuess-secretNumber)) <= (Math.abs(computerGuess-secretNumber)))
+    {
+      
+      return 'You Win!'; // User guess is closer to secret number
+    }
+  else 
+    {
+      return 'Computer Wins!';  //Computer guess is closer to secret number
+    }
+  
+}
+console.log(compareGuesses(humanGuess,computerGuess,secretNumber));
+//updating score after determining the winner
+const winner = (compareGuesses(humanGuess,computerGuess,secretNumber)=== 'You Win!' ? 'human' : 'computer');
+const updateScore = (winner) =>
+{
+  if(winner === 'human')
+    {
+      humanScore += 1;
+    }
+  else //if computer wins the game then its score will be incremented
+    {
+      computerScore += 1;
+    }
+}
+//updateScore can also be written as follows
+//const updateScore = winner => (winner === 'human') ? humanScore++ : computerScore++ ;
+updateScore(winner);
+console.log(humanScore);
+console.log(computerScore);
 
-  //This function should return a random integer between 0 and 9
-function generateTarget() {
-  
-  return Math.floor(Math.random() * 10);
-  
-  
-} //this is the end of the generateTarget() function
-
-//this function should be able to return the aboslute value between to numbers
-function getAbsoluteDistance(number1, number2) {
-  
-  return (Math.abs(number1 - number2))
-  
-} //this is the end of the getAbsoluteDistanceFunction
-
-
-//this function should compare the guesses of the player and computer with the secret number they are competing to guess at. 
-function compareGuesses(playerChoice, computerChoice, secretNumber) {
-  
-  //this should make it so that they should only type a number between 0 and 10
-  
-  if (playerChoice > 10) {
-    return alert('pick a number between 0 and 10 please')
-  }
-  
-  
-  
-  //these variables are the distance between the secret number and the choice of the computer and player
-  
-  let playerDistance = getAbsoluteDistance(playerChoice, secretNumber)
-  
-  let computerDistance = getAbsoluteDistance(computerChoice, secretNumber)
-  
-  if (playerDistance < computerDistance) {
-    
-    return true
-    
-  } else if (playerDistance > computerDistance) {
-    
-    return false
-    
-  } else if (playerDistance === computerDistance) {
-    
-    return true
-    
-  } // this is the end of the if else statement 
-  
-  
-} //this is the end of the compareGuesses() function
-
-
-
-function updateScore(winnerName) {
-  
-  if (winnerName === 'human') {
-    
-    humanScore += 1
-    
-  } else if (winnerName === 'computer') {
-    
-    computerScore += 1
-    
-  }
-  
-} // the is the end of the updateScore() function
-
-
-function advanceRound() {
-  
-  currentRoundNumber += 1
-  
-} // this is the end of the advanceRoundNumber function
+const advanceRound = () =>
+{
+  currentRoundNumber += 1;
+}
